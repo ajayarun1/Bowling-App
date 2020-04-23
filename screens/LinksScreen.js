cfa09d5f0c5f439248254b3c5da90e31337265de
ajayarun1 <ajayka2@illinois.edu>
2020-04-22 22:40:34 -0500

    Prior to user input stage

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,3 +1,4 @@
+import {Table,Row,Rows} from 'react-native-table-component'
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
@@ -5,20 +6,30 @@ import { StyleSheet, Text, View } from 'react-native';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
 export default function LinksScreen() {
+  //Need
+  var HeadTable= ['Game', 'Score', 'Frequency #', 'Conversion #', 'Conversion Rate']
+  var DataTable= [
+    ['1', '170', '25', '17', '68%'],
+    ['2', '240', '10', '6', '60%'],
+    ['3', '220', '15', '9', '60%'],
+    ['4', '180', '18', '16', '88%'],
+    ['5', '290', '20', '14', '70%']
+  ]
   return (
-    <View>
+    <View style={styles.container}>
+      <Table borderStyle={{ margin: 20, borderWidth: 3, borderColor: '#0000FF' }}>
+        <Row data={HeadTable} style={styles.HeadStyle} textStyle={styles.TableText} />
+        <Rows data={DataTable} textStyle={styles.TableText} />
+      </Table>
 
-    <Text>
-      lol
-    </Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
+    backgroundColor: '#C0C0C0',
   },
   contentContainer: {
     paddingTop: 15,
